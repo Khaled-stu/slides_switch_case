cb05af36b815e8b01bd54a2035de441f7e51162c
klod1 <klod1@LAPTOP-B1U3BL8F>
2024-11-28 23:25:24 +0300

    Add project files.

---
+++ slides_switch_case/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slides_switch_case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal;
+            
+            Console.Write(" enter your animal   ");
+            
+            switch (animal)
+            {
+                case "dog":
+                    Console.WriteLine("MAMMAL");
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    Console.WriteLine("REPTILE");
+                    break;
+                default:
+                    Console.WriteLine("There is no such animal!");
+                    break;
+            }
+        }
+    }
+}
